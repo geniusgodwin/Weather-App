6cd329893e57a0b009b9546a01e09f68dc6aead6
Godwin Kokoh <129196550+geniusgodwin@users.noreply.github.com>
2025-06-24 13:35:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const output = document.getElementById('output');
 form.addEventListener('submit', async e => {
   e.preventDefault();
   const city = document.getElementById('city').value;
-  const apiKey = 'YOUR_OPENWEATHER_API_KEY'; // Replace this!
+  const apiKey = '811523476d605f3f5bc0ba871df9ff34
 
   output.textContent = 'Loading...';
 
   try {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+    const res = await fetch(`https://api.openweathermap.org;
     const data = await res.json();
 
     if (data.cod === 200) {
